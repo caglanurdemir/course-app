649dc4afb4a99e80f8442dfb46a72305b107d654
caglanurdemir <nurcagla.demir@gmail.com>
2019-12-25 15:57:52 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    persons: [
+      { name: 'Çağla', age: 23 },
+      { name: 'Mustafa', age: 25 }
+    ],
+    otherStates: [
+      { age: 29 }
+    ]
+  }
+
+  switchNameHandler = (newName) => {
+    console.log('successfull Çağla:D:D:D');
+    this.setState({
+      persons: [
+        { name: newName, age: 23 },
+        { name: 'Mustafa', age: 25 }
+      ]
+    })
+  }
+
+  nameChangedHandler = (event) => {
+    this.setState({
+      persons: [
+        { name: 'Çağla', age: 35 },
+        { name: event.target.value, age: 35 },
+      ]
+    })
+  }
+
+  render() {
+    // const {name, age} = this.state;
+    // console.log(name);
+    // console.log(age);
+    return (
+      <div className="App">
+        <h1>Çağla'nın sitesi yeye jsx yaziiyitititti.</h1>
+        <button onClick={this.switchNameHandler.bind(this, 'Değiştirdim güzelim')}>TIKLA BEBEYİM BURAYA</button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}>Araya bir children sıkıştırdım kanki nne diyosuunnn xdxd</Person>
+        <Person
+          name={this.state.persons[1].name}
+          change={this.nameChangedHandler}></Person>
+      </div>
+    );
+
+    // return React.createElement('div', null, React.createElement('h1', null, 'Çağla\'nın sitesi yey....'))
+  }
+
 }
 
 export default App;
